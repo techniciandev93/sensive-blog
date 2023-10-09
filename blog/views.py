431b73b07af8196512f8291ad595747ebb52ac7a
anton <a.solovjev@vzv.su>
2023-10-09 17:40:27 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -44,11 +44,11 @@ def serialize_tag(tag):
 
 def index(request):
     most_popular_posts = Post.objects.popular()\
-                             .prefetch_related('author')[:5]\
+                             .prefetch_related('author', 'tags')[:5]\
                              .fetch_with_comments_count()
 
-    most_fresh_posts = reversed(Post.objects.annotate(comments_count=Count('comments')).prefetch_related('author').
-                                order_by('-published_at')[:5])
+    most_fresh_posts = reversed(Post.objects.annotate(comments_count=Count('comments'))\
+                                .prefetch_related('author', 'tags').order_by('-published_at')[:5])
     most_popular_tags = Tag.objects.popular()[:5]
 
     context = {
